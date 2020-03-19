fd10f54702654a41a972ceb438227c90c6a7166d
andreyvpng <andrewvarfolomeev@protonmail.com>
2020-03-19 16:07:29 +1000

    mode Symbols Class to Analyse.py

--- Analyse.py
+++ Analyse.py
@@ -2,7 +2,20 @@ import Settings
 
 import matplotlib.pyplot as plt
 
-from Symbols import Symbols
+class Symbols:
+    symbols_dict = dict()
+
+    def __init__(self):
+        pass
+
+    def add_symbol(self, symbol):
+        if symbol in self.symbols_dict:
+            self.symbols_dict[symbol] += 1
+        else:
+            self.symbols_dict[symbol] = 1
+
+    def get_dict(self):
+        return self.symbols_dict
 
 class Analyse:
     data = Symbols()
--- App.py
+++ App.py
@@ -5,7 +5,9 @@ from Keylogger import Keylogger
 from Analyse import Analyse
 
 def using():
-    print("Usage: python App.py {start|analys}")
+    print("Usage: python App.py {start|analys}\n")
+    print("start - start keylogger, all symbols will write in file(~/pylogger.log or $pylogger_file)")
+    print("analys - start analyse ~/pylogger and do plot and save it in ./analyse.png")
     sys.exit(2)
 
 if len(sys.argv) < 2:
@@ -14,9 +16,8 @@ if len(sys.argv) < 2:
 command = sys.argv[1]
 if command == "start":
     Keylogger().start()
-elif command == "analys":
+elif command == "analyse":
     analyse = Analyse()
     analyse.do_and_save_plot()
-    print(analyse.get_dict())
 else:
     using()
