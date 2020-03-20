f6a28cadcb492e911d0241ce1c000979e412c456
andreyvpng <andrewvarfolomeev@protonmail.com>
2020-03-20 15:28:57 +1000

    clean logic

--- analyse.py
+++ analyse.py
@@ -3,78 +3,22 @@ import matplotlib.cbook as cbook
 import matplotlib.image as mpimg
 
 import settings
-from keyboard import Qwerty
+from keyboard import Qwerty, Symbols
 
-class Symbols:
-    symbols_dict = dict()
-
-    def __init__(self):
-        pass
-
-    def add_symbol(self, symbol):
-        if symbol in self.symbols_dict:
-            self.symbols_dict[symbol] += 1
-        else:
-            self.symbols_dict[symbol] = 1
-
-    def get_dict(self):
-        return self.symbols_dict
 
 class Analyse:
-    data = Symbols()
     log_file = settings.log_file
 
     def __init__(self):
         pass
 
-    def frequency_analysis(self):
-        with open(self.log_file) as f:
-            line = f.readline()
-            while line != '':
-                line = f.readline()
-                symbol = line.rstrip()
-                self.data.add_symbol(symbol);
-
-    @staticmethod
-    def __split_array(array):
-        x, y, count = [], [], []
-        for item in array:
-            x.append(item[0])
-            y.append(item[1])
-            count.append(item[2])
-
-        return (x, y, count)
-
-    def __convert_dict_to_list(self):
-        a, data = [], self.data.get_dict()
-
-        keyboard = Qwerty()
-        data = keyboard.merge_common_buttons(data)
-
-        for item in data:
-            x, y = keyboard.get_xy_of_symbols(item)
-            if x == 0 and y == 0:
-                continue
-            a.append([x, y, data[item]])
-
-        def sortByCount(val):
-            return val[2]
-
-        a.sort(key=sortByCount, reverse=True)
-        return a
-
     def do_and_save_plot(self):
-        self.frequency_analysis();
-
-        x, y, count = self.__split_array(
-                self.__convert_dict_to_list())
+        symbols = Symbols()
+        x, y, count = Qwerty(symbols).get_scater()
 
         fig, ax = plt.subplots()
-
         ax.scatter(x, y, s=count, alpha=0.5)
-
         fig.tight_layout()
-
         img = mpimg.imread(Qwerty.image_path)
         imgplot = plt.imshow(img)
 
--- keyboard.py
+++ keyboard.py
@@ -1,22 +1,72 @@
-class Keyboard:
-    image_path_=""
+import settings
 
+class Symbols:
+    symbols_dict = dict()
+    log_file = settings.log_file
+
+    def __init__(self):
+        self.frequency_analysis()
+
+    def frequency_analysis(self):
+        with open(self.log_file) as f:
+            line = f.readline()
+            while line != '':
+                line = f.readline()
+                symbol = line.rstrip()
+                self.add(symbol)
+
+    def add(self, symbol):
+        if symbol in self.symbols_dict:
+            self.symbols_dict[symbol] += 1
+        else:
+            self.symbols_dict[symbol] = 1
+
+    def get_dict(self):
+        return self.symbols_dict
+
+class Keyboard:
     STEP = 26
     OFFSET_X = 0
     OFFSET_Y = 1
-    OFFSET = [
-        [15, 12],
-        [32, 40],
-        [36, 64],
-        [50, 90],
-        [30, 120]]
+    OFFSET = [[15, 12], [32, 40], [36, 64], [50, 90], [30, 120]]
 
-    def __init__(self):
-        pass
+    image_path_=""
+    symbols_list = []
 
-    def merge_common_buttons(self, dictionary):
+    def __init__(self, symbols):
+        self.symbols_dict = symbols
+        self.merge_common_buttons()
+        self.convert_dict_to_list()
+
+    def get_scater(self):
+        x, y, count = [], [], []
+        for item in self.symbols_list:
+            x.append(item[0])
+            y.append(item[1])
+            count.append(item[2])
+
+        return (x, y, count)
+
+    def convert_dict_to_list(self):
+      dictionary = self.symbols_dict.get_dict()
+
+      for item in dictionary:
+          x, y = self.get_coordinates_of_symbols(item)
+          if x == 0 and y == 0:
+              continue
+          self.symbols_list.append([x, y, dictionary[item]])
+
+      def sortByCount(val):
+          return val[2]
+
+      self.symbols_list.sort(key=sortByCount, reverse=True)
+
+    def merge_common_buttons(self):
         symbols_for_del = []
         symbols_for_add = dict()
+
+        dictionary = self.symbols_dict.get_dict()
+
         for item in dictionary:
             if item in self.common_button:
                 x = self.common_button[item]
@@ -34,7 +84,7 @@ class Keyboard:
 
         return dictionary
 
-    def get_xy_of_symbols(self, symbol):
+    def get_coordinates_of_symbols(self, symbol):
         for counter, row in enumerate(self.keyboard):
             if symbol in row:
                 index_of_row = counter
